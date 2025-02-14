f81cd746401b414154cf0ce01aa08a3a831fc61e
FilippaBo <filippa@bonorden.se>
2025-02-14 13:47:04 +0100

    funktions som transponerar matriser

--- matrix.py
+++ matrix.py
@@ -1,12 +1,14 @@
-def transpose():
-    transpose([[1,2]
-            ,[3,4]
-            ,[5,6]]) == [[1,3,5]
-                        ,[2,4,6]]
+def transpose(matrix):
+    rows = len(matrix) #=antal rader
+    cols =len(matrix[0])#=antal kolonner
+    new_matrix = []
     
+    for j in range(cols):
+        new_row = []
+        for i in range(rows):
+            new_row.append(matrix[i][j])  
+        new_matrix.append(new_row)  
 
-def powers():
-    powers([2,3,4],0,2) == [[1,2, 4]
-                        ,[1,3, 9]
-                        ,[1,4,16]
-                        ]
+    return new_matrix
+
+print(transpose([[1, 2], [3, 4], [5, 6]]))  
