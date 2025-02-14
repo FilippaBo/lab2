14c46767175c152501286a93e0c9171ce3101418
FilippaBo <filippa@bonorden.se>
2025-02-14 15:50:49 +0100

    def powers fungerar

--- matrix.py
+++ matrix.py
@@ -11,4 +11,25 @@ def transpose(matrix):
 
     return new_matrix
 
-print(transpose([[1, 2], [3, 4], [5, 6]]))  
+#print(transpose([[1, 2], [3, 4], [5, 6]]))  
+
+
+def powers(lst,start,end):
+    matrix=[]
+    for e in range (len(lst)):
+        
+        new_list=[]
+        
+        for exponent in range(start,end+1):
+            new_value=lst[e]**exponent
+            new_list.append(new_value)
+
+        matrix.append(new_list)
+    return matrix
+
+print(powers([2,3,4],0,2))
+
+
+
+
+#def matmul(A,B):
