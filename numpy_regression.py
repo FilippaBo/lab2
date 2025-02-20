e7512e3db537a1c5440205ea282c5f8cfac98ff8
FilippaBo <filippa@bonorden.se>
2025-02-20 11:56:48 +0100

    påbörjad poly ()

--- numpy_regression.py
+++ numpy_regression.py
@@ -24,34 +24,47 @@ def powers(lst,start,end):
 
 
 
+def polynom_regression(data,n):
+    data_transposed = transpose(data)
+    #plockar ut data ur fil
+    X = data_transposed[0]
+    Y = data_transposed[1]
+
+
+    Xp  = powers(X,0,n)
+    Yp  = powers(Y,1,1)
+    Xpt = Xp.transpose()
+
+    a = matmul(linalg.inv(matmul(Xpt,Xp)),matmul(Xpt,Yp))
+    a = a[:,0]
+
+    Y2=[]
+    for x in X: 
+        Y2.append(poly(a,x))
+    
+    plt.plot(X,Y,'ro')
+    plt.plot(X,Y2)
+    plt.show()
+
+
+def poly(a,x):
+    
+    n=len(a)
+    for i in a:
+        powers(x,0,n)
+
+
+
+
+
+
+
+
+
+
+
 data=loadtxt("chirps-modified.txt")
-data_transposed = transpose(data)
-#plockar ut data ur fil
-X = data_transposed[0]
-Y = data_transposed[1]
+polynom_regression(data)
 
-
-Xp  = powers(X,0,1)
-Yp  = powers(Y,1,1)
-Xpt = transpose(Xp)
-
-[[b],[m]] = matmul(linalg.inv(matmul(Xpt,Xp)),matmul(Xpt,Yp))
-print(b,m)
-m=float(m)
-
-b=float(b)
-
-#temperature=25
-#predicted_number_of_chirps = b + m * temperature   
-#print(predicted_number_of_chirps,b,m)
-
-Y2=[]
-for i in X: 
-    Y2.append(b + m * i) 
-
-plt.plot(X,Y,'ro')
-plt.plot(X,Y2)
-plt.show()
-
-print(powers([2,3,4],0,2))
+#print(powers([2,3,4],0,2))
 
