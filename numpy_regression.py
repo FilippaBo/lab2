cfa044908fe23241e4e7632daac29a3d479bcde0
FilippaBo <filippa@bonorden.se>
2025-02-24 13:58:01 +0100

    testade lite med att använda step vid beräkning av X2

--- numpy_regression.py
+++ numpy_regression.py
@@ -24,7 +24,7 @@ def powers(lst,start,end):
 
 
 
-def polynom_regression(data,n):
+def polynom_regression(data,n,step):
     data_transposed = transpose(data)
     #plockar ut data ur fil
     X = data_transposed[0]
@@ -39,9 +39,18 @@ def polynom_regression(data,n):
     a = a[:,0]
     
         
-    sorted_lst=X.sort()
-    X2=linspace(sorted_lst[0],sorted_lst[-1],n).tolist()
+    sorted_x=sorted(X)
 
+    """To construct X2 start with the smallest number in X and finish with the biggest.
+      You can choose n by dividing the difference between the smallest and the biggest 
+      number by a small step. In our experiments step 0.2 works well.    
+      - menar de att n=0.2 eller att vi väljer n och ett annat värde step=0.2?
+      - ska det vara så här och blir då num_steps = n och behöver vi skicka in n?"""
+    
+    num_steps = int((sorted_x[-1] - sorted_x[0]) / step) 
+    X2 = linspace(sorted_x[0], sorted_x[-1], num_steps).tolist()
+
+    
     Y2=[]
     for x in X2: 
         Y2.append(poly(a,x))
@@ -69,20 +78,8 @@ def poly(a,x):
 
 #print(poly([1,2,3],4))
 
-
-
-
-
-
-
-
-
-
-
-
-
 data=loadtxt("chirps-modified.txt")
-polynom_regression(data,0.2)
+polynom_regression(data,2,0.2)
 
 #print(powers([2,3,4],0,2))
 
